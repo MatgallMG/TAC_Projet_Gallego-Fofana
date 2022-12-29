5662cdbd339e639edfcbf8131437f7d396550342
david.fofana.etu <david.fofana.etu@univ-lille.fr>
2022-12-29 23:03:33 +0100

    Ajout du viewPager2 avec les tab / supression du fragment manager (fragment des films favoris non créé)

--- app/src/main/java/com/example/tac_projet_gallego_fofana/MainActivity.java
+++ app/src/main/java/com/example/tac_projet_gallego_fofana/MainActivity.java
@@ -1,48 +1,34 @@
 package com.example.tac_projet_gallego_fofana;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.example.tac_projet_gallego_fofana.api.API_Client;
-import com.example.tac_projet_gallego_fofana.api.API_Interface;
-import com.example.tac_projet_gallego_fofana.api.Genre;
-import com.example.tac_projet_gallego_fofana.api.GenreCatalog;
-import com.example.tac_projet_gallego_fofana.api.Movie;
-import com.example.tac_projet_gallego_fofana.api.MovieCatalog;
-import com.example.tac_projet_gallego_fofana.data.entity.FavMovie;
 import com.example.tac_projet_gallego_fofana.recycler.CustomAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class MainActivity extends AppCompatActivity {
-    private String TAG = "MATDAV";
-    private RecyclerView recyclerView;
+    //private String TAG = "MATDAV";
+    /*private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private CustomAdapter customAdapter;
     private Map<Integer, String> genre_dictionary = new HashMap<>();
-    private MainActivityViewModel mainActivityViewModel;
+    private MainActivityViewModel mainActivityViewModel;*/
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,90 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // on récupère le gestionnaire de fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        // on commence une opération (add, remove, replace,...) sur les fragments
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        // on cherche à savoir s'il y a déjà un fragment avec le 'tag' F1.
-        // Si non, on crée le fragment 1, et on affiche le fragment 1.
-        // Si oui, on ne fait rien.
-        Fragment fragmentM = fragmentManager.findFragmentByTag("Movie");
-        if (fragmentM == null) {
-            // Si ce fragment F1 n'existe pas, alors on commence par le créer
-            MovieFragment fragmentMovie = MovieFragment.newInstance();
-
-            // on demande à la transaction de l'ajouter avec le tag 'F1'
-            fragmentTransaction.add(R.id.all_Fragments, fragmentMovie, "Movie");
-            // si on garde le addToBackStack() ci-dessous, le fragment est "mémorisé" dans la pile;
-            // et donc peut être retrouvé après un 'back' plus tard.
-            //          fragmentTransaction.addToBackStack(null);
-            // Dans ce cas, quand on supprimera le fragment avec un remove, il faudra aussi
-            // appeler fragmentManager.popBackStack();  pour réellement supprimer le
-            // fragment, sinon il n'est pas détruit !
-        } else {
-            // Pour information, le fragment 1 est ici à l'écran, pas forcément visible, mais dans l'état RESUMED. Pour le savoir :
-            // Toast.makeText(MainActivity.this, fragment1.getLifecycle().getCurrentState().toString(), Toast.LENGTH_SHORT).show();
-            fragmentTransaction.show(fragmentM);
-        }
-
-        // on cherche à savoir s'il y a déjà un fragment avec le 'tag' F2 à l'écran
-        /*Fragment fragmentF = fragmentManager.findFragmentByTag("Favoris");
-        if (fragmentF != null) {
-            // on demande à la transaction de cacher ce fragment 2
-            fragmentTransaction.hide(fragmentF);
-        }
-*/
-        // on exécute la transaction (qui pourrait contenir plusieurs ordres en même temps)
-        fragmentTransaction.commit();
-
-/*
-        // affichage du fragment au clic sur le bouton Fragment Two
-        findViewById(R.id.showFragmentTwo).
-
-            setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    // on récupère le gestionnaire de fragments
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-
-                    // on commence une opération (add, remove, replace,...) sur les fragments
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-                    // on cherche à savoir s'il y a déjà un fragment avec le 'tag' F2.
-                    // Si non, on crée le fragment 2, et on affiche le fragment 2.
-                    // Si oui, on ne fait rien.
-                    Fragment fragment2 = fragmentManager.findFragmentByTag("F2");
-                    if (fragment2 == null) {
-                        // Si ce fragment F2 n'existe pas, alors on commence par le créer
-                        FragmentTwo fragmentTwo = FragmentTwo.newInstance();
-
-                        // on demande à la transaction de l'ajouter avec le tag 'F2'
-                        fragmentTransaction.add(R.id.lesFragments, fragmentTwo, "F2");
-                        // si on garde le addToBackStack() ci-dessous, le fragment est "mémorisé" dans la pile;
-                        // et donc peut être retrouvé après un 'back' plus tard.
-                        //          fragmentTransaction.addToBackStack(null);
-                        // Dans ce cas, quand on supprimera le fragment avec un remove, il faudra aussi
-                        // appeler fragmentManager.popBackStack();  pour réellement supprimer le
-                        // fragment, sinon il n'est pas détruit !
-                    } else fragmentTransaction.show(fragment2);
-
-                    // on cherche à savoir s'il y a déjà un fragment avec le 'tag' F1 à l'écran
-                    Fragment fragment1 = fragmentManager.findFragmentByTag("F1");
-                    if (fragment1 != null) {
-                        // on demande à la transaction de cacher ce fragment 2
-                        fragmentTransaction.hide(fragment1);
-                    }
-
-                    // on exécute la transaction (qui pourrait contenir plusieurs ordres en même temps)
-                    fragmentTransaction.commit();
-                }
-
-            });
-*/
+        setupViewPagerAndTabs();
     }
 
     @Override
@@ -141,4 +44,39 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Toast.makeText(this, "on resume main", Toast.LENGTH_SHORT).show();
     }
+
+    private void setupViewPagerAndTabs() {
+        viewPager = findViewById(R.id.tab_viewpager);
+        tabLayout = findViewById(R.id.menulayout);
+
+        final MovieFragment fragmentMovie = MovieFragment.newInstance();
+        //final FavFragment fragmentFavM = FragmentTwo.newInstance("B");
+
+        viewPager.setAdapter(new FragmentStateAdapter(this) {
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                if (position == 0) {
+                    return fragmentMovie;
+                }
+                return null;
+                //return fragmentFavM;
+            }
+        });
+
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(position == 0 ? MovieFragment.TAB_NAME : "Favoris"/*FavFragment.TAB_NAME*/);
+            }
+        });
+        tabLayoutMediator.attach();
+    }
+
 }
--- app/src/main/java/com/example/tac_projet_gallego_fofana/MovieFragment.java
+++ app/src/main/java/com/example/tac_projet_gallego_fofana/MovieFragment.java
@@ -47,13 +47,9 @@ public class MovieFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String TAB_NAME = "Movies";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private String TAG = "MATDAV";
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
@@ -76,36 +72,21 @@ public class MovieFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MovieFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MovieFragment newInstance(String param1, String param2) {
+    public static MovieFragment newInstance() {
         MovieFragment fragment = new MovieFragment();
+        /*
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+        */
         return fragment;
     }
 
     public MovieFragment() {
         // Required empty public constructor
-    }
-
-    public static MovieFragment newInstance() {
-        MovieFragment Mfragment = new MovieFragment();
-        return Mfragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
